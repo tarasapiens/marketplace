05daaf52fcd745e6eb822d431ce02e7c61bdb36a
tarasapiens <tarasapiens@gmail.com>
2024-02-29 03:44:29 +0300

    добавили тесты

--- src/test/java/ru/inno/market/MarketServiceTest.java
+++ src/test/java/ru/inno/market/MarketServiceTest.java
@@ -1,4 +1,182 @@
 package ru.inno.market;
 
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+import ru.inno.market.core.Catalog;
+import ru.inno.market.core.MarketService;
+import ru.inno.market.model.Client;
+import ru.inno.market.model.Item;
+import ru.inno.market.model.PromoCodes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MarketServiceTest {
+
+
+    //Тесты серии 1
+    @Test
+    @DisplayName("2.1 Создание клиента в базе")
+
+    public void createNewCleint() {
+        Client client1 = new Client(1, "Veniamin");
+        assertEquals(1, client1.getId());
+        assertEquals("Veniamin", client1.getNickname());
+    }
+
+    @Test
+    @DisplayName("1.2 Создание клиента в базе с пустым именем")
+    public void createNewClientEmpty(){
+
+        Client client2 = new Client(2, "");
+        assertTrue(StringUtils. isNotBlank(client2.getNickname()));
+    }
+
+    @Test
+    @DisplayName("1.3 Создание клиента с занятым id")
+public void createDublicateIdClient(){
+        Client client3 = new Client(3, "Tomas");
+        Client client4 = new Client(3, "Randy");
+        assertNotEquals(client3.getId(), client4.getId(), "У разных клиентов одинаковые id!");
 }
+
+//Тесты серии 2
+@Test
+@DisplayName("2.1 Создание одного заказа для одного клиента без товара")
+
+public void createOrderForClientEmpty(){
+
+    Client client1 = new Client(1, "Veniamin");
+    MarketService myMarket = new MarketService();
+    int orderId1 = myMarket.createOrderFor(client1);
+    myMarket.createOrderFor(client1);
+    assertEquals(orderId1, myMarket.getOrderInfo(1).getId());
+
+}
+    @Test
+    @DisplayName("2.2 Создание более одного заказа для одного клиента без товаров")
+    public void createSeveralOrderForClient() {
+        Client client1 = new Client(1, "Thomas");
+        MarketService myMarket = new MarketService();
+        int orderId1 = myMarket.createOrderFor(client1);
+        int orderId2 = myMarket.createOrderFor(client1);
+        assertEquals(orderId1, myMarket.getOrderInfo(1).getId());
+        assertEquals(orderId2, myMarket.getOrderInfo(2).getId());
+    }
+
+    @Test
+    @DisplayName("2.3 Создание одного заказа для одного клиента с товаром")
+    public void createOrderForClientItem(){
+        Client client1 = new Client(1, "Randy");
+        MarketService myMarket = new MarketService();
+        Catalog myCatalog = new Catalog();
+        Item myItem = myCatalog.getItemById(7);
+        int orderId = myMarket.createOrderFor(client1);
+        myMarket.addItemToOrder(myItem, orderId);
+        var myOrder = myMarket.getOrderInfo(orderId);
+        var myItems = myOrder.getItems();
+        assertEquals((Integer) 1, myItems.get(myItem));
+        System.out.println("Клиенту Randy добавлен следующий заказ: " + " " + myItems);
+    }
+
+    @Test
+    @DisplayName("2.4 Создание более одного заказа для одного клиента с товаром")
+    public void createOrdersForClientTwoItem(){
+        Client client1 = new Client(1, "Thomas");
+        MarketService myMarket = new MarketService();
+        Catalog myCatalog = new Catalog();
+
+        Item myItem1 = myCatalog.getItemById(10);
+        int orderId1 = myMarket.createOrderFor(client1);
+        myMarket.addItemToOrder(myItem1, orderId1);
+        var myOrder1 = myMarket.getOrderInfo(orderId1);
+        var myItems1 = myOrder1.getItems();
+
+        Item myItem2 = myCatalog.getItemById(1);
+        int orderId2 = myMarket.createOrderFor(client1);
+        myMarket.addItemToOrder(myItem2, orderId2);
+        var myOrder2 = myMarket.getOrderInfo(orderId2);
+        var myItems2 = myOrder2.getItems();
+        assertEquals((Integer) 1, myItems1.get(myItem1));
+        assertEquals((Integer) 1, myItems2.get(myItem2));
+
+        System.out.println("Клиенту Tomas добавлен следующий заказ: " + " " + myItems1);
+        System.out.println("Клиенту Tomas добавлен следующий заказ: " + " " + myItems2);
+    }
+
+    @Test
+    @DisplayName("2.5 Создать два одинаковых заказа с товаром для одного клиента")
+
+public  void createOrdersForClientTwoItem2(){
+        Client client1 = new Client(1, "Thomas");
+        MarketService myMarket = new MarketService();
+        Catalog myCatalog = new Catalog();
+
+        Item myItem1 = myCatalog.getItemById(10);
+        int orderId1 = myMarket.createOrderFor(client1);
+        myMarket.addItemToOrder(myItem1, orderId1);
+        var myOrder1 = myMarket.getOrderInfo(orderId1);
+        var myItems1 = myOrder1.getItems();
+
+        Item myItem2 = myCatalog.getItemById(10);
+        int orderId2 = myMarket.createOrderFor(client1);
+        myMarket.addItemToOrder(myItem2, orderId2);
+        var myOrder2 = myMarket.getOrderInfo(orderId2);
+        var myItems2 = myOrder2.getItems();
+        assertEquals((Integer) 1, myItems1.get(myItem1));
+        assertEquals((Integer) 1, myItems2.get(myItem2));
+
+        System.out.println("Клиенту Tomas добавлен следующий заказ: " + " " + myItems1);
+        System.out.println("Клиенту Tomas добавлен следующий заказ: " + " " + myItems2);
+}
+
+@Test
+@DisplayName("2.6 Создать заказ для одного клиента с несуществующим товаром (несуществующий id товара)")
+//Тут так и не разобрался, как реализовать выдачу ошибки при выборе
+//несуществующего id товара. Тест взрывается при исполнении из-за
+//попытки получить несуществующий id. По задумке это был негативный сценарий
+
+public void createOrderForClientItemFalse(){
+    Client client1 = new Client(1, "Randy");
+    MarketService myMarket = new MarketService();
+    Catalog myCatalog = new Catalog();
+    System.err.println("Задан несуществующий id товара");
+    Item myItem = myCatalog.getItemById(25);
+    int orderId = myMarket.createOrderFor(client1);
+    myMarket.addItemToOrder(myItem, orderId);
+    var myOrder = myMarket.getOrderInfo(orderId);
+    var myItems = myOrder.getItems();
+    assertEquals((Integer) 1, myItems.get(myItem));
+}
+
+    @Test
+    @DisplayName("3.1 Использовать промокод при заказе товара")
+
+        void ApplyDiscountForOneOrder() {
+
+        Client client = new Client(1, "Thomas");
+        MarketService myMarket = new MarketService();
+        int myorderId = myMarket.createOrderFor(client);
+        Catalog catalog = new Catalog();
+        Item item = catalog.getItemById(1);
+        myMarket.addItemToOrder(item, myorderId);
+
+        double totalPriceBeforeDiscount = myMarket.getOrderInfo(myorderId).getTotalPrice();
+        double discount = myMarket.applyDiscountForOrder(myorderId, PromoCodes.FIRST_ORDER);
+        double totalPriceAfterDiscount = myMarket.getOrderInfo(myorderId).getTotalPrice();
+
+        assertEquals(totalPriceBeforeDiscount * (1 - PromoCodes.FIRST_ORDER.getDiscount()), totalPriceAfterDiscount);
+        assertTrue(myMarket.getOrderInfo(myorderId).isDiscountApplied());
+    }
+}
+
+
+
+
+
+
+
+
+
+
--- src/test/java/ru/inno/market/OrderTest.java
+++ src/test/java/ru/inno/market/OrderTest.java
@@ -1,4 +1,137 @@
 package ru.inno.market;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.inno.market.core.Catalog;
+import ru.inno.market.model.Client;
+import ru.inno.market.model.Item;
+import ru.inno.market.model.Order;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class OrderTest {
+    @Test
+    @DisplayName("4.1 Добавление товара в заказ")
+
+    public void AddOneItem() {
+        Client client1 = new Client(1, "Veniamin Grushin");
+        Order myOrder = new Order(1, client1);
+        Catalog catalog = new Catalog();
+
+        Item myItem = catalog.getItemById(9);
+        myOrder.addItem(myItem);
+        assertTrue(myOrder.getItems().containsKey(myItem));
+        assertEquals(1, myOrder.getItems().get(myItem));
+        System.out.println("Пользователю Veniamin добавился следующий товар:" + " " + myItem);
+    }
+
+    @Test
+    @DisplayName("4.2 Добавить три заказа пользователю")
+    public void AddThreeItems(){
+        Client client1 = new Client(1, "Veniamin Grushin");
+        Order myOrder = new Order(1, client1);
+        Catalog catalog = new Catalog();
+
+        Item myItem1 = catalog.getItemById(9);
+        Item myItem2 = catalog.getItemById(3);
+        Item myItem3 = catalog.getItemById(10);
+        myOrder.addItem(myItem1);
+        myOrder.addItem(myItem2);
+        myOrder.addItem(myItem3);
+        assertEquals(9, myItem1.getId());
+        assertEquals(3, myItem2.getId());
+        assertEquals(10, myItem3.getId());
+    }
+
+    @Test
+    @DisplayName("4.3 Добавить заказы нескольким пользователям")
+
+    public void AddItemsUsers(){
+        Catalog catalog = new Catalog();
+        Client client1 = new Client(1, "Veniamin Grushin");
+        Client client2 = new Client(2, "Thomas");
+        Order myOrder1 = new Order(1, client1);
+        Order myOrder2 = new Order(2, client2);
+
+        Item myItem1 = catalog.getItemById(9);
+        Item myItem2 = catalog.getItemById(3);
+        myOrder1.addItem(myItem1);
+        myOrder2.addItem(myItem2);
+        assertEquals(9, myItem1.getId());
+        assertEquals(3, myItem2.getId());
+    }
+
+    @Test
+    @DisplayName("4.4 Применить скидку")
+
+    public void Discount() {
+        Client client = new Client(1, "Thomas");
+        Order myOrder = new Order(1, client);
+        Catalog catalog = new Catalog();
+
+        Item item = catalog.getItemById(4);
+        myOrder.addItem(item);
+        double totalPriceWithoutDiscount = myOrder.getTotalPrice();
+        myOrder.applyDiscount(0.3);
+        double totalPriceWithDiscount = myOrder.getTotalPrice();
+        assertEquals(totalPriceWithoutDiscount * 0.7, totalPriceWithDiscount);
+        assertTrue(myOrder.isDiscountApplied());
+    }
+
+    @Test
+    @DisplayName("4.5 Дважды применить скидку")
+
+    public void Discount2() {
+        Client client = new Client(1, "Thomas");
+        Order myOrder = new Order(1, client);
+        Catalog catalog = new Catalog();
+
+        Item item = catalog.getItemById(4);
+        myOrder.addItem(item);
+        double totalPriceWithoutDiscount = myOrder.getTotalPrice();
+        myOrder.applyDiscount(0.5);
+        myOrder.applyDiscount(0.4);//ожидаем, что вторая скидка не сработает. Тест завален, если скидка применится дважды
+        double totalPriceWithDiscount = myOrder.getTotalPrice();
+        assertEquals(totalPriceWithoutDiscount * 0.5, totalPriceWithDiscount);
+        assertTrue(myOrder.isDiscountApplied());
+    }
+
+
+    @Test
+    @DisplayName("4.6 Суммируем сумму двух заказов")
+
+    public void testGetTotalPrice() {
+        Client client1 = new Client(1, "Arnold");
+        Order myOrder = new Order(1, client1);
+        Catalog catalog = new Catalog();
+
+        Item myItem1 = catalog.getItemById(9);
+        Item myItem2 = catalog.getItemById(10);
+        myOrder.addItem(myItem1);
+        myOrder.addItem(myItem2);
+        double sumPrice = myItem1.getPrice() + myItem2.getPrice();
+        assertEquals(sumPrice, myOrder.getTotalPrice());
+        System.out.println("Общая стоимость заказа:" + " " + sumPrice);
+        System.out.println("Стоимость первого заказа:" + " " + myItem1.getPrice());
+    }
+
+    @Test
+    @DisplayName("4.7 Применить скидку на общую сумму двух товаров")
+    public void Discount3() {
+        Client client = new Client(1, "Thomas");
+        Order myOrder = new Order(1, client);
+        Catalog catalog = new Catalog();
+
+        Item item1 = catalog.getItemById(4);
+        Item item2 = catalog.getItemById(5);
+        myOrder.addItem(item1);
+        myOrder.addItem(item2);
+
+        double totalPriceWithoutDiscount = myOrder.getTotalPrice();
+        myOrder.applyDiscount(0.3);
+        double totalPriceWithDiscount = myOrder.getTotalPrice();
+        assertEquals(totalPriceWithoutDiscount * 0.7, totalPriceWithDiscount);
+        assertTrue(myOrder.isDiscountApplied());
+    }
+
 }
